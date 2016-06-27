9b96abbe58b9128f4b59c36696cffaceb4871816
Chrisliu307 <1052955698@qq.com>
2016-06-27 11:10:00 +0800

    Update run_analysis.r

--- run_analysis.r
+++ run_analysis.r
@@ -1,32 +1,23 @@
 #I have downloaded the data files and stored them in the directory: "C:/Users/Administrator/Desktop/UCI HAR Dataset"
 # 1. Merges the training and the test sets to create one data set.
 
-trainData <- read.table("C:/Users/Admin/Desktop/UCI HAR Dataset/train/X_train.txt")
-dim(trainData) # 7352*561
-head(trainData)
-trainLabel <- read.table("C:/Users/Admin/Desktop/UCI HAR Dataset/train/y_train.txt")
+trainData <- read.table("C:/Users/Administrator/Desktop/UCI HAR Dataset/train/X_train.txt")
+trainLabel <- read.table("C:/Users/Administrator/Desktop/UCI HAR Dataset/train/y_train.txt")
 table(trainLabel)
-trainSubject <- read.table("C:/Users/Admin/Desktop/UCI HAR Dataset/train/subject_train.txt")
-testData <- read.table("C:/Users/Admin/Desktop/UCI HAR Dataset/test/X_test.txt")
-dim(testData) # 2947*561
-testLabel <- read.table("C:/Users/Admin/Desktop/UCI HAR Dataset/test/y_test.txt") 
+trainSubject <- read.table("C:/Users/Administrator/Desktop/UCI HAR Dataset/train/subject_train.txt")
+testData <- read.table("C:/Users/Administrator/Desktop/UCI HAR Dataset/test/X_test.txt")
+testLabel <- read.table("C:/Users/Administrator/Desktop/UCI HAR Dataset/test/y_test.txt") 
 table(testLabel) 
-testSubject <- read.table("C:/Users/Admin/Desktop/UCI HAR Dataset/test/subject_test.txt")
+testSubject <- read.table("C:/Users/Administrator/Desktop/UCI HAR Dataset/test/subject_test.txt")
 joinData <- rbind(trainData, testData)
-dim(joinData) # 10299*561
 joinLabel <- rbind(trainLabel, testLabel)
-dim(joinLabel) # 10299*1
 joinSubject <- rbind(trainSubject, testSubject)
-dim(joinSubject) # 10299*1
 
 # Step2. Extracts only the measurements on the mean and standard 
 # deviation for each measurement. 
-features <- read.table("C:/Users/Admin/Desktop/UCI HAR Dataset/features.txt")
-dim(features)  # 561*2
+features <- read.table("C:/Users/Administrator/Desktop/UCI HAR Dataset/features.txt")
 meanStdIndices <- grep("mean\\(\\)|std\\(\\)", features[, 2])
-length(meanStdIndices) # 66
 joinData <- joinData[, meanStdIndices]
-dim(joinData) # 10299*66
 names(joinData) <- gsub("\\(\\)", "", features[meanStdIndices, 2]) # remove "()"
 names(joinData) <- gsub("mean", "Mean", names(joinData)) # capitalize M
 names(joinData) <- gsub("std", "Std", names(joinData)) # capitalize S
@@ -34,7 +25,7 @@ names(joinData) <- gsub("-", "", names(joinData)) # remove "-" in column names
 
 # Step3. Uses descriptive activity names to name the activities in 
 # the data set
-activity <- read.table("C:/Users/Admin/Desktop/UCI HAR Dataset/activity_labels.txt")
+activity <- read.table("C:/Users/Administrator/Desktop/UCI HAR Dataset/activity_labels.txt")
 activity[, 2] <- tolower(gsub("_", "", activity[, 2]))
 substr(activity[2, 2], 8, 8) <- toupper(substr(activity[2, 2], 8, 8))
 substr(activity[3, 2], 8, 8) <- toupper(substr(activity[3, 2], 8, 8))
@@ -46,7 +37,6 @@ names(joinLabel) <- "activity"
 # names. 
 names(joinSubject) <- "subject"
 cleanedData <- cbind(joinSubject, joinLabel, joinData)
-dim(cleanedData) # 10299*68
 write.table(cleanedData, "merged_data.txt") # write out the 1st dataset
 
 # Step5. Creates a second, independent tidy data set with the average of 
@@ -68,5 +58,4 @@ for(i in 1:subjectLen) {
     row <- row + 1
   }
 }
-head(result)
-write.table(result, "data_with_means.txt") # write out the 2nd dataset
+write.table(result, "data_with_means.txt") # write out the 2nd datasetwrite.table(result, "data_set_with_the_averages.txt")
